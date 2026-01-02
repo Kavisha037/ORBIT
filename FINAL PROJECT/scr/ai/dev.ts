6650be98d73b228655fdaf5cc132dc16174d71d5
Kavisha037 <kavishashukla7@gmail.com>
2026-01-02 22:09:53 +0530

    Delete FINAL PROJECT/scr directory

--- FINAL PROJECT/scr/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-personalized-daily-plan.ts';
-import '@/ai/flows/adapt-task-list-based-on-progress.ts';
-import '@/ai/flows/chatbot-flow.ts';
-import '@/ai/flows/generate-progress-insights.ts';
